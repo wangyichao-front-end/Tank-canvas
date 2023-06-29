978a45d369f6f6b713724a60585bfd55c3939473
wangyichao-front-end <1543089729@qq.com>
2023-06-29 09:53:40 +0800

    [feat]:[][绘制坦克]

--- src/ts/module/Tank.ts
+++ src/ts/module/Tank.ts
@@ -19,7 +19,7 @@ export default class Tank extends BattleCity {
     // 坦克血量
     lifeVal: number
 
-    constructor(seed:number, color:string,lifeVal:number,x?:number, y?:number ) {
+    constructor(seed: number, color: string, lifeVal: number, x?: number, y?: number) {
         super();
         this.tankW = this.config.tankWidth;
         this.tankH = this.config.tankHeight;
@@ -37,48 +37,82 @@ export default class Tank extends BattleCity {
     //开启无敌（有时间） 
     invincible(timer) {
         this.isInvincible = true;
-        setTimeout(()=>{
+        setTimeout(() => {
             this.isInvincible = false;
         }, timer)
     }
 
     //坦克移动
     //然后promise?
-    move(){
+    move = () => {
+        return new Promise((resolve) => {
+            let { canvas, tankH, tankW, hitDetection, tankX, tankY } = this;
+            const cw = canvas.width;
+            const ch = canvas.height;
+            //此处有修改¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥ 
+            const mapBottom = ch - tankH * 1.5;
+            const mapRight = cw - tankW * 1.5;
+            //移动
+            if (tankX > 0) this.dir === '左' ? tankX -= this.seed : false
+            if (tankX < mapRight) this.dir === '右' ? tankX += this.seed : false
+            if (tankY > 0) this.dir === '上' ? tankY -= this.seed : false
+            if (tankY < mapBottom) this.dir === '下' ? tankY += this.seed : false
 
+            // 遍历所以墙的位置 然后于坦克的位置进行碰撞检测
+            const moveResult = this.barrierObj.find(v => {
+                return hitDetection({
+                    x: tankX,
+                    y: tankY,
+                    w: tankW,
+                    h: tankH
+                }, v)
+            })
+            //撞到墙了
+            if(moveResult) {
+                resolve(null);
+                return;
+            }
+            // 撞到边界
+            if ((tankX <= 0 || tankX >= mapRight || tankY <= 0 || tankY >= mapBottom) && (this.color !== this.config.myTankColor)){
+                resolve(null)
+            }
+            //没有障碍物
+            this.tankX = tankX;
+            this.tankY = tankY;
+        })
     }
 
     //绘制坦克
     drawTank() {
         const x = this.tankX;
         const y = this.tankY;
-        const {ctx, tankW, tankH,color,dir} = this;
+        const { ctx, tankW, tankH, color, dir } = this;
 
         //绘制左右坦轮
         ctx.beginPath();
         ctx.save();
         ctx.fillStyle = color;
         //根据方向旋转
-        this.rotate(x, y , tankW, tankH,dir === '上' ? 0 : dir === '下' ? 180 : dir === '左' ? 270 : 90)
-        ctx.fillRect(x,y,tankW/4,tankH); //对应i=1
-        ctx.fillRect(x+(3*4/tankW),y,tankW/4,tankH); //对应 i=2
-        
-        
+        this.rotate(x, y, tankW, tankH, dir === '上' ? 0 : dir === '下' ? 180 : dir === '左' ? 270 : 90)
+        ctx.fillRect(x, y, tankW / 4, tankH); //对应i=1
+        ctx.fillRect(x + (3 * 4 / tankW), y, tankW / 4, tankH); //对应 i=2
+
+
         //轮子轮廓
         ctx.strokeStyle = "rgba(153,153,153,0.6)"
         //轮子里的横线绘制
         //2个轮子  遍历
-        for(let i = 0; i< 2 ;i++) {
+        for (let i = 0; i < 2; i++) {
             ctx.lineWidth = 1;
-            for(let k =1; k < 6;k++) {
-                const currentY = y + (tankH/5) * k; 
-                switch(i){
-                    case 1:{
-                        ctx.moveTo(x,currentY);
-                        ctx.lineTo(x+tankW/4, currentY);
+            for (let k = 1; k < 6; k++) {
+                const currentY = y + (tankH / 5) * k;
+                switch (i) {
+                    case 1: {
+                        ctx.moveTo(x, currentY);
+                        ctx.lineTo(x + tankW / 4, currentY);
                     }
-                    case 2:{
-                        ctx.moveTo(x+ tankW - tankW/4, currentY);
+                    case 2: {
+                        ctx.moveTo(x + tankW - tankW / 4, currentY);
                         ctx.lineTo(x + (tankW - tankW / 4) + tankW / 4, currentY)
                     }
 
@@ -96,10 +130,10 @@ export default class Tank extends BattleCity {
         this.borderRect(x + ((tankW / 2) - ((tankW / 6) / 2)), y - 5, tankW / 6, tankH / 1.3);
 
         //绘制无敌
-        if(this.isInvincible) {
+        if (this.isInvincible) {
             ctx.beginPath();
             ctx.strokeStyle = "rgb(255,133,0)";
-            ctx.arc(x+(tankW/2), y+(tankH/2),tankW-2,Math.PI*2, 0);
+            ctx.arc(x + (tankW / 2), y + (tankH / 2), tankW - 2, Math.PI * 2, 0);
             ctx.stroke();
             ctx.closePath();
         }
@@ -109,9 +143,9 @@ export default class Tank extends BattleCity {
 
         //绘制裂痕
         //判断是主角坦克还是敌人坦克，两种坦克血量不同
-        if(
-            this.lifeVal <= (this.color == this.config.myTankColor?this.levelParams.myTankLife/2 : this.levelParams.enemyLife/2)
-        ){
+        if (
+            this.lifeVal <= (this.color == this.config.myTankColor ? this.levelParams.myTankLife / 2 : this.levelParams.enemyLife / 2)
+        ) {
             ctx.beginPath();
             ctx.save();
             ctx.strokeStyle = "#000";
---
+++ src/ts/module/config.ts
@@ -0,0 +1,16 @@
+export default {
+    //墙的颜色
+    "wallColor":'red',
+    //墙里面线的颜色
+    "wallLineColor":'rgb(197,211,4)',
+    //坦克宽度
+    "tankWidth":28,
+    //坦克高度
+    "tankHeight":28,
+    //玩家速度
+    "myTankSeed":2,
+    //玩家颜色
+    "myTankColor":"yellow",
+    //玩家子弹速度
+    "myBulletSeed":4
+}
